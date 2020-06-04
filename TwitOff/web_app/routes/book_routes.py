eae4f52ac7e314330ec9725381c84bc8a21a3e4f
jaimieonigkeit <jaimie.onigkeit@gmail.com>
2020-06-03 19:27:49 -0600

    Book DB Works

--- TwitOff/web_app/routes/book_routes.py
+++ TwitOff/web_app/routes/book_routes.py
@@ -1,6 +1,7 @@
 # web_app/routes/book_routes.py
 
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
+from web_app.models import db, Book
 
 book_routes = Blueprint("book_routes", __name__)
 
@@ -15,11 +16,13 @@ def list_books():
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    print(book_records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
@@ -29,7 +32,9 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
+    new_book = Book(title=request.form["title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
     return jsonify({
         "message": "BOOK CREATED OK (TODO)",
         "book": dict(request.form)
